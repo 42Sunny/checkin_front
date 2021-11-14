df54945868d9b35f4679bb940155b5a398a53af0
강수호(sooohka) <shkorea1004@gmail.com>
2021-11-14 11:32:04 +0900

    CHKN-166 checkInForm 요소들 컴포넌트화

--- src/components/CheckInForm.tsx
+++ src/components/CheckInForm.tsx
@@ -4,15 +4,62 @@ import useUser from "../utils/hooks/useUser";
 import Button from "./Button";
 import List from "./List";
 
-interface IProps {
-  handleCheckIn: (e: React.FormEvent<HTMLFormElement>) => Promise<boolean>;
-}
-
 const checkLists = [
   "발열 체크시 37.5도 이하인 것을 확인했습니다.",
   "임시 출입카드 분실 시 분실 비용이 발생하는 것을 확인했습니다.",
   "마스크를 반드시 상시 착용하고 방역수칙을 준수할 것을 약속하며, 모든 설문을 이상없이 작성했음을 확인합니다.",
 ];
+
+interface CardInputProps {
+  cardNum: string;
+  handleCardNumberChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CardInput: React.FC<CardInputProps> = ({ cardNum, handleCardNumberChange }) => {
+  return (
+    <div className={classes["cardNumber-wrapper"]}>
+      <input
+        className={classes.cardNumber}
+        type='number'
+        min={1}
+        value={cardNum}
+        inputMode='numeric' /* 숫자형 키패드 */
+        placeholder='카드 번호'
+        onChange={handleCardNumberChange}
+      />
+    </div>
+  );
+};
+
+interface CheckListProps {
+  checkAll: boolean;
+  handleCheckAll: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CheckList: React.FC<CheckListProps> = ({ checkAll, handleCheckAll }) => (
+  <div className={classes["check-in-form-wrapper"]}>
+    <label htmlFor='allCheck' className={classes.allCheck}>
+      <input
+        id='allCheck'
+        type='checkbox'
+        checked={checkAll}
+        onChange={handleCheckAll}
+        className={classes.allCheckBox}
+      />
+      <span>클러스터 이용 약관에 모두 동의합니다.</span>
+      <span className={classes.asterisk}>*</span>
+    </label>
+    <ul className={classes["check-list-wrapper"]}>
+      {checkLists.map((checkList) => (
+        <List key={checkList} text={checkList} />
+      ))}
+    </ul>
+  </div>
+);
+
+interface IProps {
+  handleCheckIn: (e: React.FormEvent<HTMLFormElement>) => Promise<boolean>;
+}
 
 // const waitingNotice = [
 //   " 입장 안내 알림을 받은 후로 10분 이내에 체크인을 완료하지 않을 시에 대기가 자동 취소됨을 확인합니다.",
@@ -54,37 +101,11 @@ const CheckInForm: React.FC<IProps> = ({ handleCheckIn }) => {
 
   return (
     <form className={classes.form} onSubmit={handleCheckIn}>
-      <div className={classes["check-in-form-wrapper"]}>
-        <label htmlFor='allCheck' className={classes.allCheck}>
-          <input
-            id='allCheck'
-            type='checkbox'
-            checked={checkAll}
-            onChange={handleCheckAll}
-            className={classes.allCheckBox}
-          />
-          <span>클러스터 이용 약관에 모두 동의합니다.</span>
-          <span className={classes.asterisk}>*</span>
-        </label>
-        <ul className={classes["check-list-wrapper"]}>
-          {checkLists.map((checkList) => (
-            <List key={checkList} text={checkList} />
-          ))}
-        </ul>
-      </div>
-      <div className={classes["cardNumber-wrapper"]}>
-        <input
-          className={classes.cardNumber}
-          type='number'
-          min={1}
-          value={cardNum}
-          inputMode='numeric' /* 숫자형 키패드 */
-          placeholder='카드 번호'
-          onChange={handleCardNumberChange}
-        />
-      </div>
+      <CheckList checkAll={checkAll} handleCheckAll={handleCheckAll} />
+      <CardInput cardNum={cardNum} handleCardNumberChange={handleCardNumberChange} />
       <Button type='submit' className={classes.submitBtn} text='CHECK IN' disabled={!readySubmit} />
     </form>
   );
 };
+
 export default CheckInForm;
--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,7 +1,5 @@
 import ListIcon from "@mui/icons-material/List";
-import React, { useCallback } from "react";
-import { useHistory } from "react-router-dom";
-import { getUserStatus, postCheckIn, postCheckOut } from "../api/api";
+import React from "react";
 import classes from "../styles/ProfileCard.module.css";
 import useUser from "../utils/hooks/useUser";
 import CheckInForm from "./CheckInForm";
@@ -9,66 +7,14 @@ import CheckOutUi from "./CheckOutUi";
 
 interface IProps {
   handleFlip: (e: React.MouseEvent) => void;
+  handleCheckIn: (e: React.FormEvent<HTMLFormElement>) => Promise<boolean>;
+  handleCheckOut: () => Promise<void>;
 }
 
-const ProfileCard: React.FC<IProps> = ({ handleFlip }) => {
-  const history = useHistory();
+const ProfileCard: React.FC<IProps> = ({ handleFlip, handleCheckIn, handleCheckOut }) => {
   const {
-    user: { cardNum, state, id, profile },
-    setCardNum,
+    user: { state, id, profile },
   } = useUser();
-
-  const handleCheckIn = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      try {
-        const { data: userData } = await getUserStatus();
-        if (userData.user.card) throw new Error("이미 체크인 되었습니다.");
-        const {
-          data: { result },
-        } = await postCheckIn(cardNum);
-        if (!result)
-          throw new Error(
-            "체크인을 처리할 수 없습니다. 제한 인원 초과가 아닌 경우 관리자에게 문의해주세요.",
-          );
-        history.push("/end");
-        return true;
-      } catch (err: any) {
-        let { message } = err;
-        if (err.response?.data?.message) message = err.response.data.message;
-        setCardNum({ cardNum: "" });
-        alert(message);
-        window.location.reload();
-      }
-      return false;
-    },
-    [cardNum, history, setCardNum],
-  );
-
-  const handleCheckOut = useCallback(async () => {
-    try {
-      const { data: userData } = await getUserStatus();
-      if (!userData.user.card) throw new Error("이미 체크아웃 되었습니다.");
-      const { data } = await postCheckOut();
-      if (!data) throw new Error("무언가 잘못되었습니다.");
-
-      history.push("/end");
-    } catch (err: any) {
-      let message = "";
-      if (err.response?.data?.code === 404) {
-        message = "이미 체크아웃 되었습니다.";
-      } else if (err.response?.data?.message) {
-        console.log(err.response);
-        message = err.response.data.message;
-      } else if (err.message) {
-        message = err.message;
-      } else {
-        message = "정상적으로 처리되지 않았습니다.\n네트워크 연결 상태를 확인해주세요.";
-      }
-      alert(message);
-      window.location.reload();
-    }
-  }, [history]);
 
   return (
     <div className={classes.profileCard}>
@@ -79,11 +25,8 @@ const ProfileCard: React.FC<IProps> = ({ handleFlip }) => {
         <img className={classes.profile} src={profile} alt='profile' />
         <h2>{id}</h2>
       </div>
-      {state === "checkOut" ? (
-        <CheckInForm handleCheckIn={handleCheckIn} />
-      ) : (
-        <CheckOutUi handleCheckOut={handleCheckOut} />
-      )}
+      {state === "checkOut" && <CheckInForm handleCheckIn={handleCheckIn} />}
+      {state === "checkIn" && <CheckOutUi handleCheckOut={handleCheckOut} />}
     </div>
   );
 };
