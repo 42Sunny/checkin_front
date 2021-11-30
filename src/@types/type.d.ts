2c4e72a39c85ff176a7590f1209e159013ee5b69
강수호(sooohka) <shkorea1004@gmail.com>
2021-11-30 16:18:49 +0900

    🚨github에서 bitbucket으로 push하는 workflow수정

---
+++ src/@types/type.d.ts
@@ -0,0 +1,30 @@
+interface User {
+  isLogin: boolean;
+  id: string;
+  cardNum: string;
+  state: "checkIn" | "checkOut";
+  checkinAt: string | null;
+  checkoutAt: string | null;
+  profile: string;
+  isAdmin: boolean;
+}
+
+interface Cluster {
+  openAt: string | null;
+  closeAt: string | null;
+  gaepo: number;
+  seocho: number;
+  seochoLimitation: number;
+  gaepoLimitation: number;
+}
+
+interface Status {
+  seocho: number;
+  gaepo: number;
+}
+
+interface Log {
+  login: string;
+  date: string;
+  seconds: string;
+}
