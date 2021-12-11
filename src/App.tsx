e1269ada8cbf4275c29181849e75f1616bc49428
강수호(sooohka) <shkorea1004@gmail.com>
2021-12-11 22:21:00 +0900

    앱에 버젼 표시방식 변경 - 기존앱에 버젼을 표시할때 환경변수를 사용했는데 배포시 문제가 있음 - 이를 package.json에서 바로 불러오도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import AppRouter from "./components/AppRouter";
 import { getCookieValue } from "./utils/cookie";
 import useCluster from "./utils/hooks/useCluster";
 import useUser from "./utils/hooks/useUser";
+import { version } from "../package.json";
 
 const App = () => {
   const { setCluster } = useCluster();
@@ -43,7 +44,7 @@ const App = () => {
   return (
     <main className='wrapper'>
       <AppRouter />
-      <footer className='footer'>v{process.env.REACT_APP_VERSION}</footer>
+      <footer className='footer'>v{version}</footer>
     </main>
   );
 };
