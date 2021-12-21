5fa82c28a4dc12544dcbacfaaeba11554f06b1b2
강수호(sooohka) <shkorea1004@gmail.com>
2021-12-21 11:31:52 +0900

    checkin,out form과 ui분리, clusterChart 컴포넌트화

--- src/components/CheckInUi.tsx
+++ src/components/CheckInUi.tsx
@@ -1,37 +1,16 @@
-import { Modal, Box as MuiBox } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import { Box as MuiBox, Modal } from "@mui/material";
+import React, { useState } from "react";
 import classes from "../styles/components/CheckInUi.module.css";
 import useCluster from "../utils/hooks/useCluster";
-import Button from "./Button";
-import Checkbox from "./Checkbox";
-import List from "./List";
 import Box from "./Box";
+import CheckInForm from "./CheckInForm";
+import List from "./List";
 
 const checkLists = [
   "발열 체크시 37.5도 이하인 것을 확인했습니다.",
   "임시 출입카드 분실 시 분실 비용이 발생하는 것을 확인했습니다.",
   "마스크를 반드시 상시 착용하고 방역수칙을 준수할 것을 약속하며, 모든 설문을 이상없이 작성했음을 확인합니다.",
 ];
-
-interface CardInputProps {
-  cardNum: string;
-  handleCardNumberChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const CardInput: React.FC<CardInputProps> = ({ cardNum, handleCardNumberChange }) => (
-  <>
-    <input
-      className={classes.cardNumber}
-      type='number'
-      min={1}
-      step={1}
-      value={cardNum}
-      inputMode='decimal' /* 숫자형 키패드 */
-      placeholder='카드 번호'
-      onChange={handleCardNumberChange}
-    />
-  </>
-);
 
 const CheckList: React.FC = () => (
   <div className={classes["check-list-wrapper"]}>
@@ -48,40 +27,19 @@ interface IProps {
 }
 
 const CheckInUi: React.FC<IProps> = ({ handleCheckIn }) => {
-  const [cardNum, setCardNum] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
-  const [readySubmit, setReadySubmit] = useState(false);
   const [isOpened, setIsOpened] = useState(false);
 
   const {
     cluster: { officeHours },
   } = useCluster();
 
-  const handleModalOpen = () => {
-    setIsOpened(true);
-  };
   const handleModalClose = () => {
     setIsOpened(false);
   };
-  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setIsChecked(e.target.checked);
-  };
 
-  const handleCardNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCardNum(e.target.value);
+  const handleModalOpen = () => {
+    setIsOpened(true);
   };
-
-  const checkSubmitCondition = () => {
-    if (cardNum && isChecked) setReadySubmit(true);
-    else setReadySubmit(false);
-  };
-
-  useEffect(() => {
-    checkSubmitCondition();
-    return () => {
-      setReadySubmit(false);
-    };
-  });
 
   return (
     <>
@@ -89,30 +47,12 @@ const CheckInUi: React.FC<IProps> = ({ handleCheckIn }) => {
         <p> 클러스터 운영시간: {officeHours}</p>
         <p> 인포데스크 점심시간 13:00 ~ 14:00</p>
       </Box>
-      <form className={classes["check-in-form"]} onSubmit={handleCheckIn(cardNum)}>
-        <Modal open={isOpened} onClose={handleModalClose}>
-          <MuiBox>
-            <CheckList />
-          </MuiBox>
-        </Modal>
-        <CardInput cardNum={cardNum} handleCardNumberChange={handleCardNumberChange} />
-        <div className={classes.checkWrap}>
-          <Checkbox
-            text='방역수칙에 동의하고 입장합니다.'
-            checked={isChecked}
-            onChange={handleCheck}
-          />
-          <button type='button' className={classes.checkDetail} onClick={handleModalOpen}>
-            자세히
-          </button>
-        </div>
-        <Button
-          className={classes["check-in-button"]}
-          type='submit'
-          text='CHECK IN'
-          disabled={!readySubmit}
-        />
-      </form>
+      <Modal open={isOpened} onClose={handleModalClose}>
+        <MuiBox>
+          <CheckList />
+        </MuiBox>
+      </Modal>
+      <CheckInForm handleModalOpen={handleModalOpen} handleCheckIn={handleCheckIn} />
     </>
   );
 };
--- src/components/ClusterStatusChart.tsx
+++ src/components/ClusterStatusChart.tsx
@@ -1,56 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Doughnut } from "react-chartjs-2";
 import styles from "../styles/components/ClusterStatusChart.module.css";
 import useCluster from "../utils/hooks/useCluster";
+
+const data = (cur: number, max: number) => ({
+  datasets: [
+    {
+      data: [cur, !max ? 1 : max - cur],
+      backgroundColor: ["rgb(74, 226, 170)", "rgba(74, 226, 170, 0.2)"],
+      hoverOffset: 4,
+      borderWidth: 0,
+      cutout: "77%",
+    },
+  ],
+});
+interface ChartProps {
+  name: "개포" | "서초";
+  curUserCount: number;
+  maxUserCount: number;
+}
+const Chart: React.FC<ChartProps> = ({ name, curUserCount, maxUserCount }) => {
+  const chartData = useMemo(() => data(curUserCount, maxUserCount), [curUserCount, maxUserCount]);
+  return (
+    <div className={styles.chart}>
+      <h2 className={styles.h2}>{name}</h2>
+      <Doughnut data={chartData} />
+      <span className={styles.label}>
+        <span>
+          <strong className={styles.strong}>{curUserCount}</strong> / {maxUserCount}
+        </span>
+      </span>
+    </div>
+  );
+};
 
 const ClusterStatusChart = () => {
   const {
     cluster: { gaepo, seocho, gaepoLimitation, seochoLimitation },
   } = useCluster();
-  const dataGaepo = {
-    datasets: [
-      {
-        data: [gaepo, !gaepoLimitation ? 1 : gaepoLimitation - gaepo],
-        backgroundColor: ["rgb(74, 226, 170)", "rgba(74, 226, 170, 0.2)"],
-        hoverOffset: 4,
-        borderWidth: 0,
-        cutout: "77%",
-      },
-    ],
-  };
-
-  const dataSeocho = {
-    datasets: [
-      {
-        data: [seocho, !seochoLimitation ? 1 : seochoLimitation - seocho],
-        backgroundColor: ["rgb(88, 210, 231)", "rgba(88, 210, 231, 0.2)"],
-        hoverOffset: 4,
-        borderWidth: 0,
-        cutout: "77%",
-      },
-    ],
-  };
 
   return (
     <section className={styles.wrapper}>
-      <div className={styles.chart}>
-        <h2 className={styles.h2}>개포</h2>
-        <Doughnut data={dataGaepo} />
-        <span className={styles.label}>
-          <span>
-            <strong className={styles.strong}>{gaepo}</strong> / {gaepoLimitation}
-          </span>
-        </span>
-      </div>
-      <div className={styles.chart}>
-        <h2 className={styles.h2}>서초</h2>
-        <Doughnut data={dataSeocho} />
-        <span className={styles.label}>
-          <span>
-            <strong className={styles.strong}>{seocho}</strong> / {seochoLimitation}
-          </span>
-        </span>
-      </div>
+      <Chart curUserCount={gaepo} maxUserCount={gaepoLimitation} name='개포' />
+      <Chart curUserCount={seocho} maxUserCount={seochoLimitation} name='서초' />
     </section>
   );
 };
