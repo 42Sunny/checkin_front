7df9c776b60c5f5da915714585188c411bf37547
강수호(sooohka) <shkorea1004@gmail.com>
2021-12-21 14:52:48 +0900

    파일구조 변경
    - 컴포넌트 하위에 common, layout폴더 생성해서 파일들 분리함

--- src/components/layout/MainNotice.tsx
+++ src/components/layout/MainNotice.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styles from "../styles/components/MainNotice.module.css";
-import useCluster from "../utils/hooks/useCluster";
+import styles from "../../styles/components/MainNotice.module.css";
+import useCluster from "../../utils/hooks/useCluster";
 
 const MainNotice = () => {
   const {
